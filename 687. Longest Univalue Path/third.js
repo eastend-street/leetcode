78f815b53b230357337258f401e3d0fc1cdd84ed
eastend-street <ss_1322irf@icloud.com>
2020-01-10 14:48:54 -0800

    added 687, 596

---
+++ 687. Longest Univalue Path/third.js
@@ -0,0 +1,28 @@
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+const left = new TreeNode(4);
+left.left = 4;
+left.right = 4;
+
+const right = new TreeNode(5);
+right.right = 5;
+right.left = null;
+
+const treeNode = new TreeNode(1);
+treeNode.left = left;
+treeNode.right = right;
+
+var longestUnivaluePath = function(root) {
+  let max = 0;
+  const helper = (parentVal, childTree) => {
+    
+  };
+
+  helper(root);
+  return max;
+};
+
+console.log(longestUnivaluePath(treeNode));
