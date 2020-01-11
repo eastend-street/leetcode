1678adc06d57a0a759b6eb973c354baf89365f84
eastend-street <ss_1322irf@icloud.com>
2020-01-10 19:31:48 -0800

    added 687 third

--- 687. Longest Univalue Path/third.js
+++ 687. Longest Univalue Path/third.js
@@ -1,30 +1,55 @@
+// need to understand why it needs maxLeft and maxRight
+
 function TreeNode(val) {
   this.val = val;
   this.left = this.right = null;
 }
 
+// const left = new TreeNode(4);
+// left.left = 4;
+// left.right = 4;
+
+// const right = new TreeNode(5);
+// right.right = 5;
+// right.left = null;
+
+// const treeNode = new TreeNode(1);
+// treeNode.left = left;
+// treeNode.right = right;
 const left = new TreeNode(4);
-left.left = 4;
-left.right = 4;
+left.left = 1;
+left.right = 1;
 
 const right = new TreeNode(5);
 right.right = 5;
 right.left = null;
 
-const treeNode = new TreeNode(1);
+const treeNode = new TreeNode(5);
 treeNode.left = left;
 treeNode.right = right;
 
 var longestUnivaluePath = function(root) {
   let max = 0;
-  const helper = (node) => {
-    
+  const helper = node => {
+    if (!node) return 0;
+    let left = helper(node.left);
+    let right = helper(node.right);
+
+    let maxLeft = 0;
+    let maxRight = 0;
+    if (node.left && node.left.val === node.val) {
+      maxLeft = left + 1;
+    }
+    if (node.right && node.right.val === node.val) {
+      maxRight = right + 1;
+    }
+    max = Math.max(max, maxLeft + maxRight);
     // return is left or right
-    return 
+    return Math.max(maxLeft, maxRight);
   };
 
   helper(root);
   return max;
 };
 
-console.log(longestUnivaluePath(treeNode));
+console.log("result", longestUnivaluePath(treeNode));
