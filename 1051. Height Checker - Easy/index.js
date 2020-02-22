32c2e4bbb2c963b6bb4b6fc14552f672266fb086
eastend-street <ss_1322irf@icloud.com>
2020-02-22 15:13:01 -0800

    1051. Height Checker

---
+++ 1051. Height Checker - Easy/index.js
@@ -0,0 +1,10 @@
+// accepted
+const heightChecker = heights => {
+  let count = 0;
+  const sortedArr = heights.slice().sort((a, b) => (a < b ? -1 : 1));
+  for (let i = 0; i < heights.length; i++) {
+    if (sortedArr[i] !== heights[i]) count++;
+  }
+  return count;
+};
+console.log(heightChecker([1,2,1,2,1,1,1,2,1]));
