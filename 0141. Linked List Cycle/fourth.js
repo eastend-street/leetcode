9fcd492184077c188e2fd42658e8ae434e4ae118
eastend-street <ss_1322irf@icloud.com>
2020-02-26 20:28:43 -0800

    0141. Linked List Cycle

---
+++ 0141. Linked List Cycle/fourth.js
@@ -0,0 +1,16 @@
+// 見ないでやったパターン
+// whileを抜けたときにtrueで、while内でnullが見つかった時にfalseの方が早めにチェックできる
+const hasCycle = head => {
+  if (!head || !head.next) return false;
+  let slow = head;
+  let fast = head.next;
+
+  while (fast !== null) {
+    if (!fast.next) return false;
+    if (slow == fast) return true;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return false;
+};
