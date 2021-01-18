d6db21eaed2a18ab0815a75a63b6490f2e95c093
eastend-street <suminari.yamada@gmail.com>
2021-01-17 19:23:25 -0800

    0695. Max Area of Island

--- 0695. Max Area of Island/index.js
+++ 0695. Max Area of Island/index.js
@@ -1,38 +1,41 @@
-// https://www.youtube.com/watch?v=W8VuDt0eb5c
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const maxAreaOfIsland = (grid) => {
+  let result = 0;
+  const width = grid?.[0]?.length;
+  const height = grid.length;
 
-const maxAreaOfIsland = grid => {
-  let max = 0;
-  for (let i = 0; i < grid.length; i++) {  // gridの中身を１段ずつ見る
-    for (let j = 0; j < grid[i].length; j++) {　// gridの中身を横に1つずつ見る
-      if (grid[i][j] === 1){　// 島があった場合
-        max = Math.max(max, dfs(grid, i, j));　// 島がどこまでつながっているか深さ優先探索。現在のmaxよりも大きい場合はmaxを更新
-      } 
+  let count = 0;
+  const dfs = (grid, i, j) => {
+    if (i < 0 || j < 0 || i >= height || j >= width || grid[i][j] === 0) {
+      return;
+    }
+
+    grid[i][j] = 0;
+    count += 1;
+    dfs(grid, i - 1, j);
+    dfs(grid, i + 1, j);
+    dfs(grid, i, j - 1);
+    dfs(grid, i, j + 1);
+  };
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (grid[i][j] === 1) {
+        dfs(grid, i, j);
+        result = Math.max(result, count);
+        count = 0;
+      }
     }
   }
-  return max;
+  return result;
 };
 
-// 島があった場合、どこまで繋がっているか検索する
-const dfs = (grid, i, j) => {
-  if (!grid[i] || !grid[i][j] || grid[i][j] === 0) return 0; // gridの枠外の場合、もしくは島ではない場合（値が0の場合）は0を返す
-  let count = 1;　// 0の場合は上の行でチェックしているので、この段階では島が存在しているので+1する
-  grid[i][j] = 0;　// もう一度島があるかチェックされるのを防ぐため、0を代入して島を消す
-  count += dfs(grid, i - 1, j); // 上に向かって繋がっている島のカウントをチェック
-  count += dfs(grid, i + 1, j); // 下
-  count += dfs(grid, i, j - 1); // 左
-  count += dfs(grid, i, j + 1); // 右
-  return count;　// 島の繋がっている合計を返す
-};
-
-console.log(
-  maxAreaOfIsland([
-    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-    [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
-    [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
-  ])
-);
+const gridTest = [
+  [1, 1, 0, 0, 0],
+  [1, 1, 0, 0, 0],
+  [0, 0, 0, 1, 1],
+  [0, 0, 0, 1, 1],
+];
+console.log('result', maxAreaOfIsland(gridTest));
