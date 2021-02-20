0c4a1ac82def5f155f5320524a5e770e6c6ca4b0
eastend-street <suminari.yamada@gmail.com>
2021-02-19 23:34:55 -0800

    0111. Minimum Depth of Binary Tree

--- 0111. Minimum Depth of Binary Tree/index.js
+++ 0111. Minimum Depth of Binary Tree/index.js
@@ -1,6 +1,10 @@
-const minDepth = root => {
+// DFS but this problem should use BFS since it needs only min number. No need to look into the max depth
+const minDepth = (root) => {
   if (!root) return 0;
-  if (!root.right) return minDepth(root.left) + 1;
+
+  if (!root.right && !root.left) return 1;
   if (!root.left) return minDepth(root.right) + 1;
-  return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+  if (!root.right) return minDepth(root.left) + 1;
+  return Math.min(minDepth(root.right), minDepth(root.left)) + 1;
 };
+ï;
